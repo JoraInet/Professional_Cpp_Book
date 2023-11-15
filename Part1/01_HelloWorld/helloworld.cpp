9b106f8b5f8ca467920f0e0e24f10645ce272c48
ev <jora.inet@gmail.com>
2023-11-16 00:21:43 +0100

    Init code commit

---
+++ Part1/01_HelloWorld/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+
+int main()
+{
+	int amount;
+	std::cin >> amount;
+	for (size_t i = 0; i < amount; i++)
+	{
+		std::cout << "Hello, World!\n";
+	}
+	std::cout << "There are " << 129 << " and " << true << " ways I love you." << std::endl;
+	return 0;
+}
